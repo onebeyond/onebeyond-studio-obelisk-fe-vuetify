abd99ee0cd63e31a156cf7b5ea6b0614c978228e
jamieDavisOneBeyond <123458323+jamieDavisOneBeyond@users.noreply.github.com>
2023-06-15 12:14:52 +0100

    Vue3 tfa (#8)
    * TFA pages with Vue 3
    
    * recovery codes with vue 3
    
    * removed console log
    
    * console log
    
    ---------
    
    Co-authored-by: Jamie Davis <Jamie.Davis@DCSLSoftware.com>

--- src/src/components/tfa/disableTfa.vue
+++ src/src/components/tfa/disableTfa.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
-        <v-dialog v-model="showDisableTFA" persistent max-width="480px">
+        <v-dialog v-model="props.showDisableTFA" persistent max-width="480px">
             <v-card>
                 <v-form>
                     <v-container>
                         <v-row>
                             <v-col text cols="12">
-                                <h1>{{ $t("title") }}</h1>
+                                <h1>{{ t("title") }}</h1>
 
                                 <v-alert dense outlined type="warning">
                                     <p>
-                                        {{ $t("message") }}
-                                        <a @click="showEnableAuthenticatorCard">{{ $t("messageLink") }}</a>
+                                        {{ t("message") }}
+                                        <a @click="showEnableAuthenticatorCard">{{ t("messageLink") }}</a>
                                     </p>
                                 </v-alert>
 
                                 <div class="v-card__actions">
-                                    <v-btn @click="showTwoFactorAuthentication"> {{ $t("button.cancel") }}</v-btn>
+                                    <v-btn @click="showTwoFactorAuthentication"> {{ t("button.cancel") }}</v-btn>
                                     <v-btn color="error" class="white--text" @click="disableTFA">
-                                        {{ $t("button.disableTfa") }}
+                                        {{ t("button.disableTfa") }}
                                     </v-btn>
                                 </div>
                             </v-col>
@@ -30,36 +30,30 @@
     </div>
 </template>
 
-<script lang="ts">
-    import { Component, Vue, Prop, Emit } from "vue-property-decorator";
-    import TFAApiClient from "@js/api/tfa/tfaApiClient";
-    import dictionary from "@js/localizations/resources/components/tfa/disabletfa";
+<script setup lang="ts">
+import TFAApiClient from "@js/api/tfa/tfaApiClient";
+import dictionary from "@js/localizations/resources/components/tfa/disabletfa";
+import { useI18n } from "vue-i18n";
+import { useRouter } from "vue-router";
 
-    @Component({
-        name: "disableTfa",
-        i18n: {
-            messages: dictionary
-        }
-    })
-    export default class DisableTFA extends Vue {
-        errorMsg: string = "";
-        message: string = "";
-        tfaApiClient: TFAApiClient = new TFAApiClient();
-        @Prop() showDisableTFA!: boolean;
+const router = useRouter();
+const { t } = useI18n({
+    messages: dictionary,
+});
+const tfaApiClient = new TFAApiClient();
+const props = defineProps(["showDisableTFA"]);
+const emit = defineEmits(["showTwoFactorAuthentication", "showEnableAuthenticatorCard"]);
 
-        constructor() {
-            super();
-        }
+function showEnableAuthenticatorCard(): void {
+    emit('showEnableAuthenticatorCard');
+}
 
-        @Emit("showEnableAuthenticatorCard")
-        showEnableAuthenticatorCard(): void {}
+function showTwoFactorAuthentication(): void {
+    emit('showTwoFactorAuthentication');
+}
 
-        @Emit("showTwoFactorAuthentication")
-        showTwoFactorAuthentication(): void {}
-
-        async disableTFA(): Promise<void> {
-            await this.tfaApiClient.disableTfa();
-            this.$router.push({ name: "Dashboard" });
-        }
-    }
+async function disableTFA(): Promise<void> {
+    await tfaApiClient.disableTfa();
+    router.push({ name: "Dashboard" });
+}
 </script>
