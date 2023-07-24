fb94e9da311de3340e0a0b4f9192475a10100044
Amit Patel <amit.patel@dcslsoftware.com>
2023-07-24 14:09:00 +0100

    remove vue 2 references
    remove unused grid templates
    update language selector/user context setter

--- src/src/components/util/userContextSetter.vue
+++ src/src/components/util/userContextSetter.vue
@@ -1,42 +1,27 @@
 <template>
-    <div></div>
+    <div />
 </template>
 
-<script lang="ts">
-    import AuthApiClient from "@js/api/auth/authApiClient";
-    import { WebApiError } from "@js/api/webApiClient";
-    import { UserContext } from "@js/dataModels/users/userContext";
+<script setup lang="ts">
+    import AccountApiClient from "@js/api/accounts/accountApiClient";
+    import type { UserContext } from "@js/dataModels/users/userContext";
     import LocalSessionStorage from "@js/stores/localSessionStorage";
-    import { Vue, Component } from "vue-property-decorator";
+    import { onMounted } from "vue";
+    import { useUserContextStore } from "../../js/stores/appStore";
 
-    @Component({
-        name: "UserContextSetter",
-        components: {},
-    })
-    export default class UserContextSetter extends Vue {
-        private authApiClient: AuthApiClient;
+    const accountApiClient = new AccountApiClient();
+    const store = useUserContextStore();
 
-        constructor() {
-            super();
-            this.authApiClient = new AuthApiClient();
-        }
-
-        async created(): Promise<void> {
-            if (this.$store.state.userContext.isEmpty()) {
-                try {
-                    const userContext: UserContext = await this.authApiClient.whoAmI();
-                    this.$store.commit("setUserContext", userContext);
-                } catch (e: any) {
-                    if (e.httpCode && e.httpCode === WebApiError.UnreachableServerHttpCode) {
-                        // If server is unreachable, rethrow so that it gets caught by the global error handler
-                        throw e;
-                    } else {
-                        // Redirect to Login page if unable to get the User Context
-                        LocalSessionStorage.setUserAuthenticated(false);
-                        window.location.href = `${(window as any).location.origin}/auth/`;
-                    }
-                }
+    onMounted(async () => {
+        if (store.userContext.isEmpty()) {
+            try {
+                const userContext: UserContext = await accountApiClient.whoAmI();
+                store.setUserContext(userContext);
+            } catch {
+                // Redirect to Login page if unable to get the User Context
+                LocalSessionStorage.setUserAuthenticated(false);
+                window.location.href = `${(window as any).location.origin}/auth/`;
             }
         }
-    }
+    });
 </script>
