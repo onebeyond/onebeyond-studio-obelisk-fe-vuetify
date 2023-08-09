45be43d88ac3cd276fada32d65d2fd6a09b06c47
PeterBeyond <122605772+PeterBeyond@users.noreply.github.com>
2023-08-09 14:29:11 +0100

    Add pipeline (#45)
    * Add devops folder from syncfusion
    
    * Make any a linting error
    
    * Fix linting error

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -24,9 +24,12 @@ module.exports = {
                 ignores: ["dashboard", "users", "home"],
             },
         ],
-        "@typescript-eslint/no-explicit-any": ["warn"],
-        "vue/valid-v-slot": ['error', {
-            allowModifiers: true,
-          }],
+        "@typescript-eslint/no-explicit-any": ["error"],
+        "vue/valid-v-slot": [
+            "error",
+            {
+                allowModifiers: true,
+            },
+        ],
     },
 };
